3d28f603d77e28365a967d5be2e3efcc01a33b43
DanielMartinPerezdeLeon <114756164+DanielMartinPerezdeLeon@users.noreply.github.com>
2022-10-04 16:41:24 +0200

    Add files via upload
    Creada clase Cuenta con sus atributos y constructores

---
+++ Cuenta.java
@@ -0,0 +1,42 @@
+
+package p04;
+
+import java.util.GregorianCalendar;
+import java.util.Random;
+
+/**
+ *
+ * @author damar
+ */
+public class Cuenta {
+    private int numero;
+    private transient String propietario;
+    private float saldo;
+    private float saldominimo;
+    private GregorianCalendar fecha;
+    
+    public Cuenta(){
+        Random aleatorio = new Random();
+        this.numero=aleatorio.nextInt(99)+1;
+        this.propietario=null;
+        this.saldo=0;
+        this.saldominimo=0;
+        this.fecha=new GregorianCalendar();
+    }
+    
+    public Cuenta(int saldo, int saldominimo, String propietario){
+        this();
+        this.saldo=saldo;
+        this.saldominimo=saldominimo;
+        this.propietario=propietario;
+        
+    }
+    
+    public Cuenta(int saldo, int saldominimo, String propietario, GregorianCalendar fecha){
+        this(saldo,saldominimo,propietario);
+        this.fecha=fecha;
+        
+    }
+
+    
+}
